764eaf431daf22a711f67882c2bb911149b6c29c
React_Teaching_Project <priyassss789@gmail.com>
2022-01-30 17:36:17 +0530

    useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -6,6 +6,23 @@ import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setIserIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reacthooks-3397b-default-rtdb.firebaseio.com/ingredients.json').then(response => response.json()).then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+
+          amount: responseData[key].amount
+        });
+      }
+      setIserIngredients(loadedIngredients);
+
+    });
+  }, []);
+
 
   const addIngredientsHandler = (ingredients) => {
     fetch('https://reacthooks-3397b-default-rtdb.firebaseio.com/ingredients.json', {
