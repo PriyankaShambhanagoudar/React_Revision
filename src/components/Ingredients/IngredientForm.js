2b339249797442a470bdf63b16946663b0b6e694
React_Teaching_Project <priyassss789@gmail.com>
2022-01-30 16:00:03 +0530

    multile states

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -6,7 +6,13 @@ import "./IngredientForm.css";
 const IngredientForm = React.memo((props) => {
 
   //const inputState = useState({ title: "", amount: "" });//old way 
-  const [inputState, setInputState] = useState({ title: "", amount: "" });//new way modern js 
+  // const [inputState, setInputState] = useState({ title: "", amount: "" });//new way modern js single state
+
+  //when we have multiple states
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+
+
 
 
   const submitHandler = (event) => {
@@ -24,14 +30,23 @@ const IngredientForm = React.memo((props) => {
               type="text"
               id="title"
               // value={inputState[0].title}//old way
-              value={inputState.title}//new way modern js 
+              // value={inputState.title}//new way modern js 
+              value={enteredTitle}  //multiple states
               onChange={(event) => {
-                const newTitle = event.target.value;
-                //  inputState[1]((prevInputState) => ({   //old way
-                setInputState((prevInputState) => ({ //new way modern js 
-                  title: newTitle,
-                  amount: prevInputState.amount,
-                }));
+
+                /*  const newTitle = event.target.value;
+ 
+                  inputState[1]((prevInputState) => ({   //old way
+ 
+                // new way modern js single states
+
+                 setInputState((prevInputState) => ({
+                   title: newTitle,
+                   amount: prevInputState.amount,
+                 })); */
+
+                //multiple states
+                setEnteredTitle(event.target.value);
               }}
             />
           </div>
@@ -42,14 +57,23 @@ const IngredientForm = React.memo((props) => {
               type="number"
               id="amount"
               //   value={inputState[0].amount}//old way
-              value={inputState.amount}//new way modern js 
+              //  value={inputState.amount}//new way modern js 
+
+              value={enteredAmount} //multiple states
+
               onChange={(event) => {
-                const newAmount = event.target.value;
-                // inputState[1](prevInputState => ({//old way
-                setInputState(prevInputState => ({//new way modern js 
-                  amount: newAmount,
-                  title: prevInputState.title,
-                }));
+                /*    const newAmount = event.target.value;
+
+                   // inputState[1](prevInputState => ({//old way
+
+                 setInputState(prevInputState => ({//new way modern js
+                    amount: newAmount,
+                     title: prevInputState.title,
+                   })); */
+
+
+                //multiple states
+                setEnteredAmount(event.target.value);
               }}
             />
           </div>
