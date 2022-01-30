2bdf27e2cd71d1e95a6413a82e69412e76c1df39
React_Teaching_Project <priyassss789@gmail.com>
2022-01-30 16:54:48 +0530

    deleting the list

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import IngredientList from './IngredientList';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setIserIngredients] = useState([]);
 
   const addIngredientsHandler = (ingredients) => {
-    setIserIngredients(prevIngredients => [...prevIngredients, { id: Math.random().toString(), ...ingredients }]);
+    setIserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredients },
+    ]);
+  };
+  const removeIngredientsHandler = (ingredientsId) => {
+    setIserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredients) => ingredients.id !== ingredientsId)
+    );
   };
 
   return (
@@ -17,7 +25,7 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => { }} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientsHandler} />
       </section>
     </div>
   );
