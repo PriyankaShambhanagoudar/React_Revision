a52217606bfc49631a364bcf7a0e89ba34de72d8
React_Teaching_Project <priyassss789@gmail.com>
2022-01-30 16:47:21 +0530

    adding list

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setIserIngredients] = useState([]);
+
+  const addIngredientsHandler = (ingredients) => {
+    setIserIngredients(prevIngredients => [...prevIngredients, { id: Math.random().toString(), ...ingredients }]);
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientsHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => { }} />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
